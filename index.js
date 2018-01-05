69e0ab5b718647b265e0a5cfb007961b30a6d5ff
Jason Robertson <jason@picaboo.com>
2018-01-04 18:07:37 -0700

    pspa model

--- index.js
+++ index.js
@@ -11,21 +11,18 @@ var schema = {
         numberTotalStudents: {
             description: 'enter number of students',
             type: 'integer',
-            pattern: /^[0-9]+$/,
             message: 'Enter total number of students',
             required: true
         },
         beginGradeNumber: {
             description: 'begin grade number',
             type: 'integer',
-            pattern: /^[0-9]+$/,
             message: 'enter lower grade number to begin with',
             required: true
         },
         numberOfGrades: {
             description: 'how many grades to include',
             type: 'integer',
-            pattern: /^[0-9]+$/,
             message: 'enter number of grades',
             required: true
         },
@@ -51,19 +48,20 @@ var schema = {
         }
     }
 };
-prompt.start();
-prompt.message = '';
 
-//
-// Get two properties from the user: username and email
-//
-prompt.get(schema, function (err, result) {
+    prompt.start();
+    prompt.message = '';
+
     //
-    // Log the results.
+    // Get two properties from the user: username and email
     //
-    console.log('Command-line input received:');
-    console.log('  numStudents: ' + result.numStudents);
-    console.log('  numStudents: ' + result.numStartGrade);
+    prompt.get(schema, function (err, result) {
+        //
+        // Log the results.
+        //
+        console.log('Command-line input received:');
+        console.log('  numStudents: ' + result.numStudents);
+        console.log('  numStudents: ' + result.numStartGrade);
     });
 
 
@@ -75,4 +73,61 @@ prompt.get(schema, function (err, result) {
 
 function randomIntInc (low, high) {
     return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+
+processIndexFile_PSPA = (contents, delimiter) => {
+
+    let lines = contents.split('\n');
+
+    let portraits = [];
+    for (let i = 0; i < lines.length; i++) {
+
+        let line = lines[i].trim();
+        if (line.length === 0) {
+            continue;
+        }
+
+        let fields = line.split(delimiter);
+        let p = new Portrait();
+
+        p.volumeName = this.getStringAt(fields, 0);
+        p.imageFolder = this.getStringAt(fields, 1);
+        p.imageFileName = this.getStringAt(fields, 2);
+        p.grade = this.getStringAt(fields, 3);
+        p.lastName = this.getStringAt(fields, 4);
+        p.firstName = this.getStringAt(fields, 5);
+        p.homeRoom = this.getStringAt(fields, 6);
+        p.period = this.getStringAt(fields, 7);
+        p.teacherName = this.getStringAt(fields, 8);
+        p.track = this.getStringAt(fields, 9);
+        p.department = this.getStringAt(fields, 10);
+
+        this.addPortrait(portraits, p);
+    }
+
+    return portraits;
+};
+
+function Portrait() {
+    //Properties from PSPA spec
+    this.volumeName = '';
+    this.imageFolder = '';
+    this.imageFileName = '';
+    this.grade = '';
+    this.lastName = '';
+    this.firstName = '';
+    this.homeRoom = '';
+    this.period = '';
+    this.teacherName = '';
+    this.track = '';
+    this.department = '';
+
+    //Picaboo portrait id
+    this.id = '';
+
+    //Image stuff
+    this.imageID = '';
+    this.imageHash = '';
+    this.blob = null;
 }
